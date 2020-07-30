74a9a957688e18c16d8da5a3d624cce378f5e163
WangJialin <jialin.wang1@my.jcu.edu.au>
2020-07-30 19:25:27 +0800

    Add password check program

---
+++ password_check.py
@@ -0,0 +1,10 @@
+"""
+Check whether username is pokemon and password is go
+
+Password Check. Created by John H Waston, 30 July 2020
+"""
+
+
+def is_authorised(username,password):
+    """Check for valid username and password"""
+    return username == "pokemon" and password == "go"
